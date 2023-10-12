c131b5522710bd9bc262b0d6af1bf39e078e74ee
Hardik <hardikrajpal@cse.iitb.ac.in>
2023-10-12 23:44:27 +0530

    taking it on them backstreets together.

--- server/nlp.py
+++ server/nlp.py
@@ -1,6 +1,6 @@
 import nltk
 from nltk import word_tokenize
-from nltk import StanfordTagger, RegexpParser
+from nltk import StanfordTagger, RegexpParser, RecursiveDescentParser
 
 class SyntaxTree:
     def getTree(self,text:str):
@@ -14,15 +14,8 @@ class SyntaxTree:
         pass
 textTok = nltk.word_tokenize('The man looks for the fox.')
 posTags = nltk.pos_tag(textTok)
-
-chunker = RegexpParser("""
-                    NP: {<DT>?<JJ>*<NN>} #To extract Noun Phrases
-                    P: {<IN>}            #To extract Prepositions
-                    V: {<V.*>}           #To extract Verbs
-                    PP: {<p> <NP>}       #To extract Prepositional Phrases
-                    VP: {<V> <NP|PP>*}   #To extract Verb Phrases
-                    """)
+nltk.parse.CoreNLPParser()
+grammar = nltk.CFG.fromstring()
+rdParser = RecursiveDescentParser(grammar)
 
 # Print all parts of speech in above sentence
-output = chunker.parse(posTags)
-output.draw()
